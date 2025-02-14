037fd86933106920d926b8f66df6a9641f7de7f0
Alan Pita <pitaman512@gmail.com>
2025-02-14 10:09:03 -0600

    Import data models from omniglot-live-domains

---
+++ models/Assets.ts
@@ -0,0 +1,32 @@
+import * as Introspection from 'typescript-introspection';
+
+import { Definitions, Values } from '@pitaman71/omniglot-live-data';
+
+const __moduleName__ = 'omniglot-live-media.Assets'
+export const directory = new Definitions.Directory();
+
+export const PixelDimensionsDomain = new Values.AggregateDomain(`${__moduleName__}.PixelDimensionsDomain`, {
+    width: Values.TheNumberDomain,
+    height: Values.TheNumberDomain
+})
+directory.add(PixelDimensionsDomain);
+
+export interface _Asset {
+    error?: string,
+    mime?: string,
+    pixelDimensions?: Introspection.getValueType<typeof PixelDimensionsDomain>,
+    uri?: string,
+    optimize?: Introspection.JSONValue   
+}
+
+/**
+ * A media file, such as a video or image, along with critical
+ * metadata.
+ */
+export const AssetDomain = new Values.AggregateDomain<_Asset>(`${__moduleName__}.AssetDomain`, {
+    mime: Values.TheStringDomain,
+    pixelDimensions: PixelDimensionsDomain,
+    uri: Values.TheStringDomain,
+    optimize: Values.TheJSONDomain
+}, ['optimize']);
+directory.add(AssetDomain);
---
+++ models/index.ts
@@ -0,0 +1,9 @@
+import { Definitions } from '@pitaman71/omniglot-live-data';
+
+import * as Assets from './Assets';
+
+export const directory = Definitions.Directory.from(
+    Assets.directory
+)
+
+export { Assets };
